a9a84e3ef9634bbd7522bf1eea963e73be02a192
Celia <joyihirwecelia@gmail.com>
2026-09-24 14:24:16 +0200

    Java learning

---
+++ Java-journey/PlayableInterface/Guitar.java
@@ -0,0 +1,7 @@
+class Guitar implements Playable{
+
+    @Override
+    public void play(){
+        System.out.println("Playing guitar...");
+    }
+}
---
+++ Java-journey/PlayableInterface/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        Guitar guitar = new Guitar();
+        guitar.play();     
+    }
+}
